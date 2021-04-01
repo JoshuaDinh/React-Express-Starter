0a9820079bca3208e82e6970d81c3b84390efda4
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-01 14:16:55 -0700

    added customer api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Customers from "./components/customers/Customers";
 
 const App = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/customers")
+      .then((res) => res.json())
+      .then((customers) => setCustomers(customers));
+  }, []);
+
+  console.log(customers);
   return (
     <div className="App">
       <header className="App-header">
